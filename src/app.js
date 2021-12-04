6c59ae637f27ff6a0c82a1f0de5edbbf6c3cacb5
nikoloza <nika.tomadze@gmail.com>
2021-12-04 05:24:04 +0400

    version init

---
+++ src/app.js
@@ -0,0 +1,17 @@
+'use strict'
+
+import React, { useEffect, useRef, useState } from 'react'
+import ReactDOM from 'react-dom'
+
+import { Button } from '@symbo.ls/react'
+
+function App () {
+  const [text, setText] = useState(0)
+  return <>
+    <input type="text" value={text} onChange={e => setText(e.target.value)} />
+    <Button text={text} setText={setText} size='C' padding="A" />
+  </>
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
