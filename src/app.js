1bf34c5fbbad25a6c8e9f8bb5800a8b0f346419b
nikoloza <nika.tomadze@gmail.com>
2021-12-05 18:56:48 +0400

    fixed new symbols api

--- src/app.js
+++ src/app.js
@@ -1,19 +1,20 @@
 'use strict'
 
-
 import React, { useEffect, useRef, useState } from 'react'
 import ReactDOM from 'react-dom'
 
-import '@domql/emotion'
-import { Button } from '@symbo.ls/react'
+import { Button, DatePicker, Input, Flex } from '@symbo.ls/react'
 
 function App () {
   const [text, setText] = useState('test text')
   return <>
     <input type="text" value={text} onChange={e => setText(e.target.value)} />
-    <Button text={text.length} setText={setText} size='C' padding="A">
-      <span>{text} and lenght: </span>
-    </Button>
+    <Flex flow="column" gap="A">
+      <Input value={text.length} setText={setText} onInput={e => setText(e.target.value) } />
+      <Button text={text.length} setText={setText} theme="primary" size='B' padding="Z B" round="C">
+        <span>{text} and lenght: </span>
+      </Button>
+    </Flex>
   </>
 }
 
