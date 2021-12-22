d730830c29f57fdc92b9fb9ed483abec32255450
nikoloza <nika.tomadze@gmail.com>
2021-12-22 06:26:26 +0400

    new api

--- src/app.js
+++ src/app.js
@@ -3,18 +3,22 @@
 import React, { useEffect, useRef, useState } from 'react'
 import ReactDOM from 'react-dom'
 
-import { Button, DatePicker, Input, Flex } from '@symbo.ls/react'
+import { Button, DatePicker, Input, Flex, SquareButton } from '@symbo.ls/react'
 
 function App () {
   const [text, setText] = useState('test text')
   return <>
-    <input type="text" value={text} onChange={e => setText(e.target.value)} />
+    <input type="text" value={text} onChange={e => setText(e.target.value)} /> {text.length}
     <Flex flow="column" gap="A">
-      <Input value={text.length} setText={setText} onInput={e => setText(e.target.value) } />
-      <Button text={text.length} setText={setText} theme="primary" size='B' padding="Z B" round="C">
+      <Input value={text.length} onInput={e => setText(e.target.value) } />
+      <Button text={text.length} theme="primary" size='B' padding="Z B" round="C">
         <span>{text} and lenght: </span>
       </Button>
     </Flex>
+
+    <DatePicker></DatePicker>
+
+    <SquareButton />
   </>
 }
 
