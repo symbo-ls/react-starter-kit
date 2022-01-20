06be34fed0bdad53b868f6b4a7c9aeab6d5e2bf0
nikoloza <nika.tomadze@gmail.com>
2022-01-21 00:44:00 +0400

    text render

--- src/app.js
+++ src/app.js
@@ -4,18 +4,37 @@ import React, { useEffect, useRef, useState } from 'react'
 import ReactDOM from 'react-dom'
 import PATH from 'path'
 
-import { Button, DatePicker, Input, Flex } from '@symbo.ls/react'
+import { Button, Grid, Notification, DatePicker, Input, Flex } from '@symbo.ls/react'
+import { DOMQLReact } from '@domql/transform-react'
+
+export const Test = (props, state) => {
+  return DOMQLReact({
+    // button: Button,
+    asd: 'asd',
+    text: 'Test'
+  }, props, state)
+}
 
 function App () {
   const [text, setText] = useState('test text')
   return <>
-    <input type="text" value={text} onChange={e => setText(e.target.value)} />
-    <Flex flow="column" gap="A">
-      <Input value={text.length} setText={setText} onInput={e => setText(e.target.value) } />
-      <Button text={text.length} setText={setText} theme="primary" size='B' padding="Z B" round="C">
+    {/* <input type="text" value={text} onChange={e => setText(e.target.value)} /> */}
+    {/* <Flex flow="column" gap="A">
+      <Input value={text.length} onInput={e => setText(e.target.value) } />
+      <Button text={text.length} onClick={e => setText('Reset')} theme="primary" size='B' padding="Z B" round="C">
         <span>{text} and lenght: </span>
       </Button>
     </Flex>
+
+    <Grid>
+      <Notification theme="primary" />
+      <Notification theme="primary">
+        asd
+      </Notification>
+    </Grid> */}
+
+    <Test />
+    {/* <Test>AA {text}</Test> */}
   </>
 }
 
